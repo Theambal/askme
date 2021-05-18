7f8b7cdd467d631e6fb6edfd30db25ee75403d7b
Андрей <47117541+Theambal@users.noreply.github.com>
2021-05-18 10:16:08 +0300

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
   validates :text, :user, presence: true
-  validates :text, length: { maximum: 500 }
+  validates :text, length: { maximum: 255 }
 end
